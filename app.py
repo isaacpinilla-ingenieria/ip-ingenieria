bee4dbd35060a8daafc045fbdeb71e0245ca662f
isaacpinilla-ingenieria <isaacpinilla2129@gmail.com>
2026-03-18 16:44:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -61,8 +61,7 @@ def cotizar():
     import os
 
     if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
