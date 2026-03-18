8dd163c9ec90863fd3f799a72e97e23172e90670
isaacpinilla-ingenieria <isaacpinilla2129@gmail.com>
2026-03-18 16:26:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,11 @@ def cotizar():
         'success'
     )
     return redirect(url_for('index') + '#cotizacion')
+    import os
+
+    if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     app.run(debug=True)
